bd9aa21fa6411d6d0b81b7f4fc226bc76a745f6f
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-11-09 15:18:26 +0700

    using services for pushing data from A to B

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import { Ingredient } from './../shared/ingredient.model';
 
 export class ShoppingListService {
+
+  ingredientChanged = new EventEmitter<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -13,5 +16,6 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientChanged.emit(this.ingredients.slice());
   }
 }
