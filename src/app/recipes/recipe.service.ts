b5b6fef8c3856aa00a8ff9f4ca04e59580639169
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-11-09 15:32:57 +0700

    adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -6,8 +7,23 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Recipe Name', 'This is recipe', 'https://p0.pikist.com/photos/666/25/food-meat-recipe-power-pork-dishes.jpg'),
-    new Recipe('Recipe Name 2', 'This is second recipe', 'https://p0.pikist.com/photos/666/25/food-meat-recipe-power-pork-dishes.jpg')
+    new Recipe(
+      'Coffee Latte',
+      'A Good Coffee Latte for Your Amazing Day',
+      'https://www.bermberm.com/wp-content/uploads/2018/09/Cafe-latte-Berm-Berm-Coffees.jpg',
+      [
+        new Ingredient('Espresso', 1),
+        new Ingredient('Fresh Milk', 1)
+      ]),
+    new Recipe(
+      'Macchiato Coffee',
+      'A Perfect Combination Coffee and Caramel',
+      'https://www.caffesociety.co.uk/assets/recipe-images/macchiato-small.jpg',
+      [
+        new Ingredient('Espresso', 1),
+        new Ingredient('Fresh Milk', 1),
+        new Ingredient('Caramel', 1)
+      ])
   ];
 
   getRecipes() {
