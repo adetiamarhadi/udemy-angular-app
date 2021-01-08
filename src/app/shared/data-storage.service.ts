e60f2f1acd149c1161ea0e80669c73a9f7d06047
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-01-08 10:26:52 +0700

    fetching recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { RecipeService } from './../recipes/recipe.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -16,6 +17,15 @@ export class DataStorageService {
       recipes
     ).subscribe(response => {
       console.log(response);
+    });
+  }
+
+  fetchRecipes() {
+
+    this.http.get<Recipe[]>(
+      'https://ng-course-recipe-book-a3cd0-default-rtdb.firebaseio.com/recipes.json'
+    ).subscribe(recipes => {
+      this.recipeService.setRecipes(recipes);
     });
   }
 }
