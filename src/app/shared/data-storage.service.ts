900fca5a916883e8fa1ff012032842f28ab506bd
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-01-08 10:05:01 +0700

    storing recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,21 @@
+import { RecipeService } from './../recipes/recipe.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+
+    const recipes = this.recipeService.getRecipes();
+
+    this.http.put(
+      'https://ng-course-recipe-book-a3cd0-default-rtdb.firebaseio.com/recipes.json',
+      recipes
+    ).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
