ee618000ddf898f83f7b93763b0eff1bb7699d2d
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-01-11 14:24:17 +0700

    adding a loading spinner & error handling logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { RecipeService } from './recipes/recipe.service';
 import { AppRoutingModule } from './app-routing.module';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
@@ -32,7 +33,8 @@ import { AuthComponent } from './auth/auth.component';
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
-    AuthComponent
+    AuthComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -10,6 +10,8 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent implements OnInit {
 
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   constructor(private authService: AuthService) { }
 
@@ -29,6 +31,7 @@ export class AuthComponent implements OnInit {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
     if (this.isLoginMode) {
       // ...
     } else {
@@ -36,8 +39,11 @@ export class AuthComponent implements OnInit {
       this.authService.signup(email, password).subscribe(
         responseData => {
           console.log(responseData);
+          this.isLoading = false;
         }, error => {
           console.log(error);
+          this.error = "An error occureed!";
+          this.isLoading = false;
         }
       );
     }
