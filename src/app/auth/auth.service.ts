12fb6192552f6268182ab7bf380a7f98ac0e5220
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-01-11 14:54:57 +0700

    sending login requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
 
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: string;
 }
 
 @Injectable({providedIn: 'root'})
@@ -40,6 +41,18 @@ export class AuthService {
           return throwError(errorMessage);
         }
       )
+    );
+  }
+
+  login(email: string, password: string) {
+
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCEhxNJ_wUMHRg14DcP4ejkN_5ozIVrvKc',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
     );
   }
 }
