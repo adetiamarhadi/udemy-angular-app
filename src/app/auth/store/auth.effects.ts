6dc295ba6344133bf567bd5f25cdf623e1206dc6
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-01-25 16:18:18 +0700

    effects & error handling

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,11 +1,45 @@
-import { Actions, ofType } from '@ngrx/effects';
-import * as AuthActions from './auth.actions'
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { from, of } from 'rxjs';
+import { switchMap, catchError, map } from 'rxjs/operators';
+import * as AuthActions from './auth.actions';
+
+export interface AuthResponseData {
+
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: string;
+}
 
 export class AuthEffects {
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private http: HttpClient) {}
 
+  @Effect()
   authLogin = this.actions$.pipe(
-    ofType(AuthActions.LOGIN_START)
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http.post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
+        {
+          email: authData.payload.email,
+          password: authData.payload.password,
+          returnSecureToken: true
+        }
+      ).pipe(
+        catchError(error => {
+          // ...
+          of();
+        }),
+        map(resData => {
+          of();
+        })
+      );
+    }),
+
   );
 }
