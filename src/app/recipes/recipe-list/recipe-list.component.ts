178983c22a8144b2afe4810beee01e0f02bfc2d3
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-01-26 11:44:43 +0700

    getting started with ngrx for recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,10 @@
+import * as fromApp from './../../store/app.reducer';
 import { Subscription } from 'rxjs';
-import { RecipeService } from './../recipe.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from './../recipe.model';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-list',
@@ -14,19 +16,21 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
   subscription: Subscription;
 
-  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute,
+    private store: Store<fromApp.AppState>) { }
 
   ngOnInit(): void {
-    this.subscription = this.recipeService.recipeChanged.subscribe(
+    this.subscription = this.store.select('recipes')
+    .pipe(map(recipesState => recipesState.recipes))
+    .subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
       }
     );
-    this.recipes = this.recipeService.getRecipes();
   }
 
   onNewRecipe() {
-    this.router.navigate(['new'], {relativeTo: this.route});
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 
   ngOnDestroy() {
